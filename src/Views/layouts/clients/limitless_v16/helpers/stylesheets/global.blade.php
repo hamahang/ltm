448ec7075980f6eb1f1966495c51f008671127b6
faramarz <faramarz.sadeghi89@gmail.com>
2023-07-15 09:47:12 +0330

    add fullcalender

--- src/Views/layouts/clients/limitless_v16/helpers/stylesheets/global.blade.php
+++ src/Views/layouts/clients/limitless_v16/helpers/stylesheets/global.blade.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('vendor/laravel_task_manager/build/client/css/init_core.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/laravel_task_manager/packagegs/fullcalander/css/fullcalendar.css') }}">
 {{--<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/client_custom_style.css') }}">--}}
 
 <style>
